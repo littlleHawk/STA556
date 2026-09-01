f86bf03cedd0be1054324b5c4992114657916e08
Sakina <119966698+littlleHawk@users.noreply.github.com>
2026-09-01 21:43:14 +0000

    Complete Week 1 workflow activity

---
+++ src/hello_sta556.py
@@ -0,0 +1,10 @@
+import sys
+
+print("Hello from STA 556!")
+
+print("\nWhat is your name?")
+name = input()
+
+print(f"\n\nWelcome to the course, {name}.")
+print("This is the Fall semester 556 course")
+print(f"Runnin python version {sys.version}")
