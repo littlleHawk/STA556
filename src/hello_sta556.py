bace147e7303544fd76bbc4ac4e28e4cd3505924
Sakina <119966698+littlleHawk@users.noreply.github.com>
2026-09-01 21:46:10 +0000

    Add workflow message to Week 1 script

--- src/hello_sta556.py
+++ src/hello_sta556.py
@@ -8,3 +8,8 @@ name = input()
 print(f"\n\nWelcome to the course, {name}.")
 print("This is the Fall semester 556 course")
 print(f"Runnin python version {sys.version}")
+
+
+print(
+    "I am beginning to build reproducible computational workflows."
+)
